c69d75f793e6eff24335dd8811f6d7e68a0e93ea
samynaj <samuelnnj@gmail.com>
2022-04-23 14:58:34 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SectionWrapper } from './components'
+import assets from './assets'
+import styles from './styles/Global';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper 
+        title='Your own store of Nifty NFTs. Start Selling and Growing.'
+        description='Buy, store, collect NFTs, exchange & earn crypto. Join 25+ million using Profnef Marketplace.'
+        showBtn
+        mockupImg={assets.homeHero}
+        banner='banner'
+
+      />
+      <SectionWrapper 
+        title='Smart User Interface Marketplace'
+        description='Experience buttery UI of Pronef NFT Marketplace. Smooth constant colors of a fluent UI design.'
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+
+      <SectionWrapper 
+        title='Deployment'
+        description="ProNef is built using expo which runs natively on all users' devices. You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+
+      />
+      <SectionWrapper 
+        title='Creative Way to Showcase the Store'
+        description="The app contains two screens, the first screen lists all NFTs while the second screen shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner='banner02'
+      />
+      <Download />
+
+      <div className='px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04'>
+        <p className={`${styles.pText} ${styles.whiteText}`}>Designed by {' '}
+        <span className='bold'>Samynaj</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
